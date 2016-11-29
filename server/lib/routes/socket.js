5a1bbac18272c13353f074532299ac1719538d6a
Kevin Pacheco <kev.pachec.lis@gmail.com>
2016-11-29 15:49:06 -0600

    Refactorizada la forma en la que se manejan los movimientos

--- server/lib/routes/socket.js
+++ server/lib/routes/socket.js
@@ -14,10 +14,11 @@ var rectThickness = 2;
 var lowThresh = 0;
 var highThresh = 50;
 var nIters = 2;
-var minArea = 500;
+var minArea = 600;
 
 // initialize camera
 var camera = new cv.VideoCapture(0);
+
 camera.setWidth(camWidth);
 camera.setHeight(camHeight);
 
@@ -43,6 +44,7 @@ module.exports = function (socket) {
       //Compute the absolute difference between the current frame and first frame
       var frameDelta = new cv.Matrix(im.width(), im.height());
       frameDelta.absDiff(firstFrame,gray);
+
       // dilate the thereshold image to fill un holes
       // then find contours on threasholder image
       frameDelta.canny(lowThresh, highThresh);
@@ -53,22 +55,20 @@ module.exports = function (socket) {
   	  for (i = 0; i < contours.size(); i++) {
   	  	//if te contour is too small, ignore it
     	if (contours.area(i) < minArea) continue;
-    	
-    	//im.rectangle([contour.x, contour.y], [contour.width, contour.height], rectColor, rectThickness);
-    	console.log("Movement");
+
+    	im.detectObject('./lib/cascade/cars.xml', {}, function(err, faces) {
+        	if (err) throw err;
+
+        	for (var i = 0; i < faces.length; i++) {
+          		face = faces[i];
+          		im.rectangle([face.x, face.y], [face.width, face.height], rectColor, rectThickness);
+        	}
+
+      	});
       }
 
+      socket.emit('frame', { buffer: im.toBuffer() });
 
-      im.detectObject('./lib/cascade/cars.xml', {}, function(err, faces) {
-        if (err) throw err;
-
-        for (var i = 0; i < faces.length; i++) {
-          face = faces[i];
-          im.rectangle([face.x, face.y], [face.width, face.height], rectColor, rectThickness);
-        }
-
-        socket.emit('frame', { buffer: im.toBuffer() });
-      });
     });
   }, camInterval);
 };
